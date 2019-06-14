b03351f66f7b0cacb21c2918705853c9a3a9ebe0
zhoucc <zhoucc@sunmnet.com>
2019-06-14 10:11:50 +0800

    配置静态资源访问路径

--- src/main/java/com/example/multidatasourcedemo/configure/MvcConfig.java
+++ src/main/java/com/example/multidatasourcedemo/configure/MvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -37,5 +38,13 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(acessInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/register");
+    }
+
+    /**
+     * 这个方法是用来配置静态资源的，比如html，js，css，等等
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
