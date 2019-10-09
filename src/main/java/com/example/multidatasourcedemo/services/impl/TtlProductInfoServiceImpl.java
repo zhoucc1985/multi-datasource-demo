7e2c685f1f58bd9446d31ccf3121b385ab78586c
zhoucc <zhoucc@sunmnet.com>
2019-10-09 18:31:32 +0800

    Excel 表格导出修改表头

--- src/main/java/com/example/multidatasourcedemo/services/impl/TtlProductInfoServiceImpl.java
+++ src/main/java/com/example/multidatasourcedemo/services/impl/TtlProductInfoServiceImpl.java
@@ -52,27 +52,27 @@ public class TtlProductInfoServiceImpl implements TtlProductInfoService {
      */
     private List<ExcelHeaderInfo> getHeaderInfo() {
         return Arrays.asList(
-                new ExcelHeaderInfo(1, 1, 0, 0, "id"),
-                new ExcelHeaderInfo(1, 1, 1, 1, "商品名称"),
+                new ExcelHeaderInfo(0, 0, 0, 0, "id"),
+                new ExcelHeaderInfo(0, 0, 1, 1, "商品名称"),
 
-                new ExcelHeaderInfo(0, 0, 2, 3, "分类"),
-                new ExcelHeaderInfo(1, 1, 2, 2, "类型ID"),
-                new ExcelHeaderInfo(1, 1, 3, 3, "分类名称"),
+//                new ExcelHeaderInfo(0, 0, 2, 3, "分类"),
+                new ExcelHeaderInfo(0, 0, 2, 2, "类型ID"),
+                new ExcelHeaderInfo(0, 0, 3, 3, "分类名称"),
 
-                new ExcelHeaderInfo(0, 0, 4, 5, "品牌"),
-                new ExcelHeaderInfo(1, 1, 4, 4, "品牌ID"),
-                new ExcelHeaderInfo(1, 1, 5, 5, "品牌名称"),
+//                new ExcelHeaderInfo(0, 0, 4, 5, "品牌"),
+                new ExcelHeaderInfo(0, 0, 4, 4, "品牌ID"),
+                new ExcelHeaderInfo(0, 0, 5, 5, "品牌名称"),
 
-                new ExcelHeaderInfo(0, 0, 6, 7, "商店"),
-                new ExcelHeaderInfo(1, 1, 6, 6, "商店ID"),
-                new ExcelHeaderInfo(1, 1, 7, 7, "商店名称"),
+//                new ExcelHeaderInfo(0, 0, 6, 7, "商店"),
+                new ExcelHeaderInfo(0, 0, 6, 6, "商店ID"),
+                new ExcelHeaderInfo(0, 0, 7, 7, "商店名称"),
 
-                new ExcelHeaderInfo(1, 1, 8, 8, "价格"),
-                new ExcelHeaderInfo(1, 1, 9, 9, "库存"),
-                new ExcelHeaderInfo(1, 1, 10, 10, "销量"),
-                new ExcelHeaderInfo(1, 1, 11, 11, "插入时间"),
-                new ExcelHeaderInfo(1, 1, 12, 12, "更新时间"),
-                new ExcelHeaderInfo(1, 1, 13, 13, "记录是否已经删除")
+                new ExcelHeaderInfo(0, 0, 8, 8, "价格"),
+                new ExcelHeaderInfo(0, 0, 9, 9, "库存"),
+                new ExcelHeaderInfo(0, 0, 10, 10, "销量"),
+                new ExcelHeaderInfo(0, 0, 11, 11, "插入时间"),
+                new ExcelHeaderInfo(0, 0, 12, 12, "更新时间"),
+                new ExcelHeaderInfo(0, 0, 13, 13, "记录是否已经删除")
         );
     }
 
